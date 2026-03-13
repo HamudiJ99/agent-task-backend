88adb4407af8ace98cdb58ade73f5fc1117b5dca
Hamudij99 <hamudij8@gmail.com>
2026-03-13 13:07:03 +0100

    setup and dependencys prepared

---
+++ src/main/java/com/example/agent_task/AgentTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.agent_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agent_task/AgentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agent_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
